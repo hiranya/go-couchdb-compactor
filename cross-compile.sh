7e99275f0b905411ea465bab18c9b784cf0ef10d
Hiranya Samarasekera <hiranyas@gmail.com>
2016-07-25 09:12:46 +0530

    Added amd64 binary for linux

--- cross-compile.sh
+++ cross-compile.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 env GOOS=linux GOARCH=386 go build -o ./binaries/go-couchdb-compactor-linux-386 compactor.go
+env GOOS=linux GOARCH=amd64 go build -o ./binaries/go-couchdb-compactor-linux-amd64 compactor.go
 env GOOS=darwin GOARCH=386 go build -o ./binaries/go-couchdb-compactor-mac-386 compactor.go
